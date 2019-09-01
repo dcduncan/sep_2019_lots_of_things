69b78dcef3040db5d76284eb932a3390503b4286
Daniel Duncan <dcduncan.code@gmail.com>
2019-08-31 21:09:45 -0700

    Add Kotlin server

---
+++ src/main/kotlin/com/dj/app/MainApp.kt
@@ -0,0 +1,19 @@
+package com.dj.app
+
+import io.ktor.application.call
+import io.ktor.http.ContentType
+import io.ktor.response.respondText
+import io.ktor.routing.get
+import io.ktor.routing.routing
+import io.ktor.server.engine.embeddedServer
+import io.ktor.server.netty.Netty
+
+fun main(args: Array<String>) {
+    embeddedServer(Netty, 8080) {
+        routing {
+            get("/") {
+                call.respondText("Poop", ContentType.Text.Html)
+            }
+        }
+    }.start(wait = true)
+}
