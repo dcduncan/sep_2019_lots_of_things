e99bce271dc11cceff4dfa77a4566203056ec80b
Daniel Duncan <dcduncan.code@gmail.com>
2019-08-31 21:34:28 -0700

    Add React

--- src/main/kotlin/com/dj/app/MainApp.kt
+++ src/main/kotlin/com/dj/app/MainApp.kt
@@ -2,17 +2,19 @@ package com.dj.app
 
 import io.ktor.application.call
 import io.ktor.http.ContentType
+import io.ktor.response.respondFile
 import io.ktor.response.respondText
 import io.ktor.routing.get
 import io.ktor.routing.routing
 import io.ktor.server.engine.embeddedServer
 import io.ktor.server.netty.Netty
+import java.io.File
 
 fun main(args: Array<String>) {
     embeddedServer(Netty, 8080) {
         routing {
             get("/") {
-                call.respondText("Poop", ContentType.Text.Html)
+                call.respondFile(File("./js.dagger2/src/index.html"))
             }
         }
     }.start(wait = true)
